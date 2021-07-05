17940d2bb3d08c9d9d5f57f8f6587eb28060e8ca
Mohak-Goel <1705417@kiit.ac.in>
2021-07-05 16:51:55 +0530

    First local repo commit

---
+++ MainCollection.java
@@ -0,0 +1,7 @@
+package companyname.projectname.modulename;
+public class MainCollection {
+    public static void main(String[] args) {
+        UserService userService = new UserService();
+        userService.process();
+    }
+}
---
+++ Xyz.java
@@ -0,0 +1,15 @@
+package companyname.projectname.modulename;
+interface Process{
+    void add(int i);
+}
+
+abstract class AddProcess{
+    abstract void add(int i);
+}
+
+public class Xyz extends AddProcess implements Process{
+    @Override
+    public void add(int i) {
+        System.out.println("--"+i);
+    }
+}
